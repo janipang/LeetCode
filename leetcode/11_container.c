47950a6166d9acb005e24eb0ae851044cdf13fe7
janipang <pang.janista@gmail.com>
2025-05-25 22:58:42 +0700

    pending: container

---
+++ leetcode/11_container.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int min(int a, int b){
+    return a < b ? a : b;
+}
+
+int maxArea(int* height, int heightSize) {
+    int max_area = 0;
+    int cur_area = 0;
+    for ( int start = 0; start < heightSize - 1; start++){
+        for ( int stop = start + 1; stop < heightSize; stop ++){
+            cur_area = (stop - start) * min(height[start], height[stop]);
+            // printf("start %d, stop %d, height1 %d, height2 %d, area %d\n", start, stop, height[start], height[stop], cur_area);
+            if (cur_area > max_area) max_area = cur_area;
+        }
+        printf("---------------\n");
+    }
+    return max_area;
+}
+
+int main()
+{
+    int length = 9;
+    int array[100] = {1,8,6,2,5,4,8,3,7};
+    // printf("test1: %d\n", maxArea(array, length));
+    return 0;
+}

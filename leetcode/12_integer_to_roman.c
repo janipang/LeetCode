d5279fadb9f04b72fb3c2c0fb6f824f93d82b9b6
janipang <pang.janista@gmail.com>
2025-06-04 01:44:00 +0700

    solved: int to roman

--- leetcode/12_integer_to_roman.c
+++ leetcode/12_integer_to_roman.c
@@ -1,5 +1,14 @@
 # include <stdio.h>
 
+void append_str(char* dest, const char* src, int* index) {
+    while (*src) {
+        dest[*index] = *src;
+        (*index)++;
+        src++;
+    }
+    dest[*index] = '\0';
+}
+
 int power(int base ,int power){
     if (power == 0) return 1;
     else{
@@ -32,55 +41,59 @@ char* get_symbol(int digit_number, int digit_value, int i){
     // ((i-1)*2) +1if5  = {0,   1,   2,   3,   4,   5,   6};
     // index        = {0,   1,   2,   3,   4,   5,   6};
     char symbols[8] = {'I', 'V', 'X', 'L', 'C', 'D', 'M'};
-    static char result[10] = "";
-    printf("%p\n", &result);
+    static char result[10];
     int result_index = 0;
-    if (digit_number == 4 || digit_number == 9){
-        result[result_index] = get_symbol(1, digit_value, i)[0];
+    int value = digit_number * digit_value;
+    while (value > 0){
+        int isFive = 0;
+        if (value >= 5 * digit_value){
+            isFive = 1;
+            value -= 5 * digit_value;
+        }
+        else{
+            value -= 1 * digit_value;
+        }
+        int symbol_index = ((i - 1) * 2) + isFive;
+        result[result_index] = symbols[symbol_index];
         result_index ++;
-        if (digit_number == 4){
-            result[result_index] = get_symbol(5, digit_value, i)[0];
-            result_index ++;
-        }
-        if (digit_number == 9){
-            result[result_index] = get_symbol(1, digit_value * 10, i + 4)[0];
-            result_index ++;
-        }
-        result_index ++;
-        result[result_index] = '\0';
-    }else{
-        int value = digit_number * digit_value;
-        while (value > 0){
-            int isFive = 0;
-            if (value >= 5 * digit_value){
-                isFive = 1;
-                value -= 5 * digit_value;
-            }
-            else{
-                value -= 1 * digit_value;
-            }
-            int symbol_index = ((i - 1) * 2) + isFive;
-            result[result_index] = symbols[symbol_index];
-            result_index ++;
-        }
-        result[result_index] = '\0';
     }
+    result[result_index] = '\0';
+    return result;
+}
+
+char* get_symbol_49(int digit_number, int digit_value, int i){
+    char prefix = get_symbol(1, digit_value, i)[0];
+    char suffix;
+    static char result[10];
+    int result_index = 0;
+    if (digit_number == 4) suffix = get_symbol(5, digit_value, i)[0];
+    if (digit_number == 9) suffix = get_symbol(1, digit_value * 10, i + 1)[0];
+    result[result_index ++] = prefix;
+    result[result_index ++] = suffix;
+    result[result_index] = '\0';
     return result;
 }
 
 char* intToRoman(int num) {
     int digit = count_digit(num);
+    static char result[100];
+    int result_index = 0;
+    char *temp;
     for (int i = digit; i > 0; i--){
-        int digit_value = power(10, i-1); //ค่าประจำหลัก
-        int digit_number = num /= digit_value; //เลขที่หลักนั้น
-        get_symbol(digit_number, digit_value, i);
+        int digit_value = power(10, i-1);
+        int digit_number = (num / digit_value) % 10;
+        // printf("digit_value: %d, digit_number: %d, i: %d\n", digit_number,digit_value, i); 
+        if(digit_number == 4 || digit_number == 9) temp = get_symbol_49(digit_number, digit_value, i);
+        else temp = get_symbol(digit_number, digit_value, i);
+        append_str(result, temp, &result_index);
     }
+    // printf("result: %s", result);
+    return result;
 }
 
 int main(){
-    printf("%s", get_symbol(4,1,1));
-//     printf("test1: %d\n", intToRoman(3749)); // -> MMMDCCXLIX
-//     printf("test2: %d\n", intToRoman(58)); // -> LVIII
-//     printf("test3: %d\n", intToRoman(1994)); // -> MCMXCIV
+    // printf("test1: %s\n", intToRoman(3749)); // -> MMMDCCXLIX
+    // printf("test2: %s\n", intToRoman(58)); // -> LVIII
+    printf("test3: %s\n", intToRoman(1994)); // -> MCMXCIV
 }
     

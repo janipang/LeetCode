c4cf24acad9c670eab4b02e481fe6b8147b8343c
janipang <pang.janista@gmail.com>
2025-07-27 00:09:54 +0700

    solving: min_max_critical

--- leetcode/2058_min_max_critical.c
+++ leetcode/2058_min_max_critical.c
@@ -13,7 +13,6 @@ int *nodesBetweenCriticalPoints(struct ListNode *head, int *returnSize)
     int *result = (int *)malloc(*returnSize * sizeof(int));
     if (!head || !head->next || !head->next->next)
     {
-        printf("break");
         result[0] = -1;
         result[1] = -1;
         return result;
@@ -21,24 +20,35 @@ int *nodesBetweenCriticalPoints(struct ListNode *head, int *returnSize)
     int curr_point = 1;
     int *critical_points = (int *)malloc(100000 * sizeof(int)); // keep index of critical point
     int critical_amount = 0;
+    int min_dis = 100000;
     while (head && head->next && head->next->next)
     {
-        printf("round -> %d %d %d", head->val, head->next->val, head->next->next->val);
         if ((head->val > head->next->val && head->next->val < head->next->next->val) || (head->val < head->next->val && head->next->val > head->next->next->val))
         {
-            printf(" ---> found %d", head -> next -> val);
             *(critical_points + critical_amount) = curr_point;
             critical_amount += 1;
+            // re calculate min distance
+            if (critical_amount > 1)
+            {
+                if (curr_point - *(critical_points + critical_amount - 2) < min_dis)
+                {
+                    min_dis = curr_point - *(critical_points + critical_amount - 2);
+                }
+            }
         }
         curr_point += 1;
         head = head->next;
-        printf("\n");
     }
     printf("critical_points: ");
     for (int i = 0; i < critical_amount; i++)
     {
         printf("%d", *(critical_points + i));
     }
+    printf("\n");
+
+    result[0] = min_dis;
+    result[1] = *(critical_points + critical_amount - 1) - *(critical_points);
+    printf("result: %d %d\n",result[0],result[1]);
     return result;
 }
 
@@ -51,14 +61,18 @@ int main()
     struct ListNode node5;
     struct ListNode node6;
     struct ListNode node7;
-    
-    node1.val = 5;
+    struct ListNode node8;
+    struct ListNode node9;
+
+    node1.val = 1;
     node2.val = 3;
-    node3.val = 1;
+    node3.val = 2;
     node4.val = 2;
-    node5.val = 5;
-    node6.val = 1;
+    node5.val = 3;
+    node6.val = 2;
     node7.val = 2;
+    node8.val = 2;
+    node9.val = 7;
 
     node1.next = &node2;
     node2.next = &node3;
@@ -66,7 +80,9 @@ int main()
     node4.next = &node5;
     node5.next = &node6;
     node6.next = &node7;
-    node7.next = NULL;
+    node7.next = &node8;
+    node8.next = &node9;
+    node9.next = NULL;
 
     int *returnSize;
 
